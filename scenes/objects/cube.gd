62263cdfeec33bb7f4a986a6d856893c0a77b825
jiexdrop <jorge.noogueira@gmail.com>
2025-09-09 17:46:48 +0100

    fixes puzzle

--- scenes/objects/cube.gd
+++ scenes/objects/cube.gd
@@ -4,16 +4,21 @@ extends StaticBody3D
 # List of Marker3D nodes
 @export var markers: Array[NodePath] = []
 
+var _last_index: int = -1  # stores the last chosen marker index
+
 func _ready():
 	randomize()
-	# Move to a random marker at start
 	move_to_random_marker()
 
 func move_to_random_marker():
-	if markers.size() == 0:
+	if markers.is_empty():
 		return
 
-	# Pick a random marker
-	var marker_node = get_node(markers[randi() % markers.size()]) as Marker3D
+	var new_index: int = _last_index
+	while new_index == _last_index and markers.size() > 1:
+		new_index = randi() % markers.size()
+
+	_last_index = new_index
+	var marker_node = get_node(markers[new_index]) as Marker3D
 	if marker_node:
 		global_transform.origin = marker_node.global_transform.origin
