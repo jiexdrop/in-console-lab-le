01f1e21548535b814f0f7763fdb2c4e5b4de3f6a
jiexdrop <jorge.noogueira@gmail.com>
2025-09-17 18:01:03 +0100

    improving playability with hints

--- scripts/tool_calls_2.gd
+++ scripts/tool_calls_2.gd
@@ -22,8 +22,6 @@ func _ready() -> void:
 	var level2_node = get_tree().root  # This should be Level1
 	sunny = level2_node.find_child("Sunny", true, false)
 	player_2ainpc = sunny.find_child("Player2AINPC", true, false)
-	lever_1 = level2_node.find_child("Lever", true, false)
-	lever_1_target = level2_node.find_child("Lever1Target", true, false)
 	door1_target = level2_node.find_child("Door1Target", true, false)
 	end_goal_target = level2_node.find_child("EndGoalTarget", true, false)
 	door_a = level2_node.find_child("Door_A", true, false)
@@ -33,17 +31,6 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	# Check if we're waiting for Sunny and if she's reached the door destination
-	if waiting_for_lever and sunny and lever_1_target:
-		var distance = sunny.global_position.distance_to(lever_1_target.global_position)
-		if distance <= sunny.stop_distance * 2:
-			print("Sunny reached destination - opening door!")
-			if not lever_1_state:
-				lever_1.activate_lever()
-			else:
-				lever_1.deactivate_lever()
-			waiting_for_lever = false
-			
 	if waiting_for_sunny and sunny and door1_target:
 		var distance = sunny.global_position.distance_to(door1_target.global_position)
 		if distance <= sunny.stop_distance * 2:
@@ -59,17 +46,6 @@ func _process(delta: float) -> void:
 			print("Sunny reached end_goal destination!")
 			waiting_for_end_goal = false
 
-## Will activate/deactivate lever 1
-func toggle_lever_1() -> void:
-	print("Telling Sunny to move to bridge target...")
-	
-	# Tell Sunny to move to the bridge target
-	if sunny and lever_1_target:
-		sunny.move_to_position(lever_1_target.global_position)
-		waiting_for_lever = true
-		lever_1_state = not lever_1_state
-	else:
-		print("Could not find Sunny or Bridge1Target")
 	
 ## Will open the door once Sunny reaches the target
 func open_door() -> void:
